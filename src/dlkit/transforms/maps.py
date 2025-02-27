83a45a9acac57fe3b1349e024be1422c505a3aed
constatza <29166969+constatza@users.noreply.github.com>
2025-02-27 14:08:59 +0200

    simplified Map class

--- src/dlkit/transforms/maps.py
+++ src/dlkit/transforms/maps.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
-from dlkit.transforms.base import Invertible
+
+from dlkit.transforms.base import Invertible, Map
 
 
 epsilon = 1e-8
@@ -15,18 +16,11 @@ class Log1pSigned(Invertible):
         """
         return torch.log1p(torch.abs(x)) * torch.sign(x)
 
-    def inverse(self) -> nn.Module:
+    def inverse_module(self) -> nn.Module:
         """
         The inverse of the `direct` transformation.
 
         That is, the function maps y to sign(y) * (exp(|y|) - 1), where exp denotes the natural exponential function.
         """
 
-        class Inverse(nn.Module):
-            def __init__(self):
-                super().__init__()
-
-            def forward(self, y: torch.Tensor) -> torch.Tensor:
-                return torch.expm1(torch.abs(y)) * torch.sign(y)
-
-        return Inverse()
+        return Map(lambda y: torch.sign(y) * (torch.expm1(torch.abs(y))))
