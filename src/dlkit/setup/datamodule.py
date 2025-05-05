e2d7d384f40faf6a7a7b9987a3dc3ca43ed83433
constatza <29166969+constatza@users.noreply.github.com>
2025-05-05 12:20:41 +0300

    refactor: create base class for datamodule and remove idx_split handling from datamodule

--- src/dlkit/setup/datamodule.py
+++ src/dlkit/setup/datamodule.py
@@ -1,47 +1,59 @@
 from typing import Literal
 
 from dlkit.utils.system_utils import import_dynamic
-from dlkit.datamodules.in_memory import InMemoryModule
-from dlkit.settings.general_settings import DatamoduleSettings, PathSettings
+from dlkit.settings.general_settings import DataSettings, PathSettings
 from dlkit.setup.transforms import initialize_transforms
 from dlkit.transforms.chaining import Pipeline
+from dlkit.datamodules.base import BaseDataModule
+from dlkit.datamodules.utils import get_or_create_idx_split
 
 
 def initialize_datamodule(
-    datamodule_settings: DatamoduleSettings,
+    data_settings: DataSettings,
     paths: PathSettings,
     datamodule_device: Literal["cpu", "cuda"] = "cpu",
-) -> InMemoryModule:
+) -> BaseDataModule:
     """
     Dynamically imports and sets up the datamodule based on the provided configuration.
     :return: LightningDataModule: The instantiated datamodule object.
     """
 
     feature_transforms: Pipeline = initialize_transforms(
-        datamodule_settings.feature_transforms
+        data_settings.feature_transforms
     )
 
-    target_transforms: Pipeline = initialize_transforms(
-        datamodule_settings.target_transforms
-    )
+    target_transforms: Pipeline = initialize_transforms(data_settings.target_transforms)
 
     dataset = import_dynamic(
-        datamodule_settings.dataset.name,
-        prepend=datamodule_settings.dataset.module_path,
+        data_settings.dataset.name,
+        prepend=data_settings.dataset.module_path,
     )
 
     dataset = dataset(
-        **datamodule_settings.dataset.to_dict_compatible_with(dataset),
+        **data_settings.dataset.to_dict_compatible_with(dataset),
         **paths.to_dict_compatible_with(dataset),
     )
 
-    datamodule_instance = InMemoryModule(
+    module = import_dynamic(
+        data_settings.module.name,
+        prepend=data_settings.module.module_path,
+    )
+
+    idx_split = get_or_create_idx_split(
+        n=len(dataset),
+        filepath=paths.idx_split,
+        save_dir=paths.input_dir,
+        test_size=data_settings.test_size,
+        val_size=data_settings.val_size,
+    )
+
+    datamodule_instance = module(
         dataset=dataset,
-        settings=datamodule_settings,
-        paths=paths,
+        settings=data_settings,
         device=datamodule_device,
         features_pipeline=feature_transforms,
         targets_pipeline=target_transforms,
+        idx_split=idx_split,
     )
 
     return datamodule_instance
