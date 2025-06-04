6c16bd6e925be44dd0669ed88abb3c3524d2b25b
constatza <29166969+constatza@users.noreply.github.com>
2025-06-04 20:05:24 +0300

    feature: add train_state_from_path for ease

--- src/dlkit/run/training.py
+++ src/dlkit/run/training.py
@@ -2,6 +2,9 @@ from .vanilla_training import train_vanilla
 from .mlflow_training import train_mlflow
 from .optuna_training import train_optuna
 from loguru import logger
+from dlkit.io.settings import load_validated_settings
+from dlkit.datatypes.learning import TrainingState
+from dlkit.settings import Settings
 
 
 def train(settings, optuna: bool = False, mlflow: bool = False):
@@ -12,3 +15,8 @@ def train(settings, optuna: bool = False, mlflow: bool = False):
         logger.info("Using MLflow for logging.")
         return train_mlflow(settings)
     return train_vanilla(settings)
+
+
+def train_state_from_path(settings_path: str) -> tuple[TrainingState, Settings]:
+    settings = load_validated_settings(settings_path)
+    return train_vanilla(settings), settings
