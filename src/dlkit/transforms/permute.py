32a11fef6fc57d5e008c573380f687de5b4207ed
constatza <29166969+constatza@users.noreply.github.com>
2025-06-05 20:31:45 +0300

    refactor: maps should be spread into separate files

---
+++ src/dlkit/transforms/permute.py
@@ -0,0 +1,22 @@
+import torch
+
+from dlkit.transforms.base import Transform
+
+epsilon = 1e-8
+
+
+class Permutation(Transform):
+    def __init__(self, dims: tuple[int]):
+        """Must be a permutation and must be used before any other transforms."""
+        super().__init__()
+        self.dims = dims
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return x.permute(self.dims)
+
+    def inverse_transform(self, y: torch.Tensor) -> torch.Tensor:
+        inverse_dims = [0] * len(self.dims)
+        for i, dim in enumerate(self.dims):
+            inverse_dims[dim] = i
+
+        return y.permute(*inverse_dims)
