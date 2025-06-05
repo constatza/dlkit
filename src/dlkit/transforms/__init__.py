7667435fc33734aab1ac5618f2d402eab40a82a0
constatza <29166969+constatza@users.noreply.github.com>
2025-06-05 22:31:02 +0300

    fix: persistent standard scaler

--- src/dlkit/transforms/__init__.py
+++ src/dlkit/transforms/__init__.py
@@ -2,14 +2,14 @@ from .chain import TransformChain
 from .permute import Permutation
 from .minmax import MinMaxScaler
 from .pca import PCA
-from .standard import StdScaler
+from .standard import StandardScaler
 from .subset import TensorSubset
 
 __all__ = [
     "TransformChain",
     "MinMaxScaler",
     "PCA",
-    "StdScaler",
+    "StandardScaler",
     "Permutation",
     "TransformChain",
     "TensorSubset",
